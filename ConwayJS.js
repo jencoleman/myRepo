7c5f707490cd2d5834285f6c0ac47459d03f2bf5
jencoleman <jencolemanjd@gmail.com>
2015-01-01 18:56:51 -0700

    Loop to generate grid

--- ConwayJS.js
+++ ConwayJS.js
@@ -1,21 +1,21 @@
 var gridBuild = function(a, b) {
 
-	document.write( " <div id= \"c" + i + "\" class = \"column\">" );
-
-	for (i = 1 ; i <= a ; i++) {
-
-		for(z = 1; i <= b; i++) {
+	for (i = 0; i < a ; i++) {
 		
-			document.write( " <div id=\"c" + i + "r" + z + \" class=\"cell\"></div>" );
-
-			}
-			
+		document.write("<div id=\"c" + i + "\" class=\"column\">");
+	
+		for ( z = 0; z < b; z++) {
+		
+			document.write("<div id=\"c" + i + "r" + z + "\" class=\"cell\">lorem</div>");
+		
 		}
-		
-		document.write( "</div>" );
+	
+		document.write("</div>");
+	
 	}
-
 
 }
 
-getElementById("grid").innerHTML = gridBuild(100, 50);
+var gridVariable = document.getElementById("grid");
+
+gridVariable = gridBuild(10, 10);
