6752b49a5311b212df6e567cfb4dd0d69dd6e3df
jencoleman <jencolemanjd@gmail.com>
2015-01-03 17:49:18 -0700

    Update ConwayJS.js

--- ConwayJS.js
+++ ConwayJS.js
@@ -43,3 +43,21 @@ content.push("</div>");
 }
 
 setHtml();
+
+function setHtml() {
+var content = "";
+var elem = document.getElementById("grid");
+
+ for (i = 0; i < 10 ; i++) {
+ content += "<div id=\"c" + i + "\" class=\"column\">";
+for ( z = 0; z < 10; z++) {
+content +="<div id=\"c" + i + "r" + z + "\" class=\"cell\">lorem</div>";
+}
+content += "</div>";
+}
+}
+ elem.innerHTML = content;
+}
+
+setHtml();
+
