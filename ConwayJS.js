48f800819f92bc50b372c549902bec680bba6b55
jencoleman <jencolemanjd@gmail.com>
2015-01-04 20:27:42 -0700

    Update ConwayJS.js

--- ConwayJS.js
+++ ConwayJS.js
@@ -3,22 +3,34 @@ var content = "";
 
 var cellCounter = 0;
 
-var createGrid = function(a, b) {
-	
-	for (i = 0; i < a ; i++) {
-		
+var createGrid = function() {
+
+	for (i = 0; i < 10 ; i++) {
+
 		content += "<div id=\"c" + i + "\" class=\"column\">";
 		
-		for ( z = 0; z < b; z++) {
-			
-			content +=("<div id=\"c" + i + "r" + z + "\" class=\"cell\">" + cellCounter + "</div>");
-			
-			cellCounter++
+		for ( z = 0; z < 10; z++) {
+
+			content +=("<div id=\"c" + i + "r" + z + "\" class=\"cell\">" + cellCounter+ "</div>");
+
+			cellCounter++;
+
 		}
+
 		content +=("</div>");
+
 	}
-return content;
+	
+	return content;
+	
 }
+
+createGrid();
+
+var elem = document.getElementById("grid");
+
+elem.innerHTML = content;
+
 //Creates the cell array
 var cellArray = [];
 
