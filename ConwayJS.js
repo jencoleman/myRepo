10c20876e948812b27c53db0362f9c5702dda0d9
jencoleman <jencolemanjd@gmail.com>
2015-01-05 21:00:43 -0700

    Cleaner code

--- ConwayJS.js
+++ ConwayJS.js
@@ -1,9 +1,9 @@
 //Creates the grid
-var content = "";
-
-var cellCounter = 0;
 
 var createGrid = function() {
+	
+	var cellCounter = 0;
+	var content = "";
 
 	for (i = 0; i < 10 ; i++) {
 
@@ -12,7 +12,6 @@ var createGrid = function() {
 		for ( z = 0; z < 10; z++) {
 
 			content +=("<div id=\"c" + i + "r" + z + "\" class=\"cell\">" + cellCounter+ "</div>");
-
 			cellCounter++;
 
 		}
@@ -20,7 +19,6 @@ var createGrid = function() {
 		content +=("</div>");
 
 	}
-	
 	return content;
 	
 }
@@ -50,99 +48,3 @@ var createArray = function() {
 }
 
 createArray();
-
-/*
-var content = function() {
-}
-	for (i = 0; i < 10 ; i++) {
-	("<div id=\"c" + i + "\" class=\"column\">");
-	
-	for ( z = 0; z < 10; z++) {
-	("<div id=\"c" + i + "r" + z + "\" class=\"cell\">lorem</div>");
-	}
-		("</div>");
-}
-}
-
-document.getElementById("grid").innerHTML = content();
-
-//2nd one
-var content = "";
-
-var someFunction = function() {
-	for (i = 0; i < 10 ; i++) {
-	content.push("<div id=\"c" + i + "\" class=\"column\">");
-	
-	for ( z = 0; z < 10; z++) {
-	content.push("<div id=\"c" + i + "r" + z + "\" class=\"cell\">lorem</div>");
-	}
-		content.push("</div>");
-}
-}
-
-document.getElementById("grid").innerHTML = content;
-
-
-//3rd one
-function setHtml() {
-var content = "";
-var elem = document.getElementById("grid");
-
- for (i = 0; i < 10 ; i++) {
-content.push("<div id=\"c" + i + "\" class=\"column\">");
-for ( z = 0; z < 10; z++) {
-content.push("<div id=\"c" + i + "r" + z + "\" class=\"cell\">lorem</div>");
-}
-content.push("</div>");
-}
-}
- elem.innerHTML = content;
-}
-
-setHtml();
-
-//4th one
-function setHtml() {
-var content = "";
-var elem = document.getElementById("grid");
-
- for (i = 0; i < 10 ; i++) {
- content += "<div id=\"c" + i + "\" class=\"column\">";
-for ( z = 0; z < 10; z++) {
-content +="<div id=\"c" + i + "r" + z + "\" class=\"cell\">lorem</div>";
-}
-content += "</div>";
-}
-}
- elem.innerHTML = content;
-}
-
-setHtml();
-
-5th one
-var content = "";
-
-var someFunction = function(a, b) {
-
-	for (i = 0; i < a ; i++) {
-
-		content += "<div id=\"c" + i + "\" class=\"column\">";
-
-		for ( z = 0; z < b; z++) {
-
-			content +=("<div id=\"c" + i + "r" + z + "\" class=\"cell\">lorem</div>");
-
-}
-
-*/
-
-		content +=("</div>");
-
-}
-
-}
-someFunction();
-
-var elem = document.getElementById("grid");
-
-elem.innerHTML = content;
