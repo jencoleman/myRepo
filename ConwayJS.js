6cf0a650bf0c02cdefd1718c3cf858b5a372945e
jencoleman <jencolemanjd@gmail.com>
2015-01-07 20:42:54 -0700

    Fixed "a" to a; merged array, grid functions

--- ConwayJS.js
+++ ConwayJS.js
@@ -1,8 +1,9 @@
 //Create grid & array
 
-
 var createArrayGrid = function() {
 
+	var elem = document.getElementById("grid");
+	
 	var content = "";
 	
 	var cellCounter = 0;
@@ -13,83 +14,27 @@ var createArrayGrid = function() {
 	
 		content += "<div id=\"c" + i + "\" class=\"column\">";
 		
-	for ( z = 0; z < 10; z++) {
+		for ( z = 0; z < 10; z++) {
 	
-		content +="<div id=\"[" + i + "][" + z + "]" + "\" class=\"cell\">" + "cellCounter" + "</div>";
-cellCounter++;
-cellArray.push("[" + i + "][" + z + "]");
-}
-content +=("</div>");
-}
-return content;
-}
+				content +="<div id=\"[" + i + "][" + z + "]" + "\" class=\"cell\">" + cellCounter + "</div>";
+				cellCounter++;
+				cellArray.push("[" + i + "][" + z + "]");
+			}
+			content +=("</div>");
+		}
+	elem.innerHTML = content;
+	}
+	
 
 createArrayGrid();
-var elem = document.getElementById("grid");
-elem.innerHTML = content;
 
 //Change cell class to live
 var itsAlive = function(a){
 	
-	var myCell = document.getElementById("a");
+	var myCell = document.getElementById(a);
 	
 	myCell.className = "live";
 	
 }
 
 itsAlive("[2][2]");
-
-/*
-//Creates the grid
-
-var createGrid = function() {
-	
-	
-
-	for (i = 0; i < 10 ; i++) {
-		
-		var cellCounter = 0;
-		
-		var content = "";
-
-		content += "<div id=\"c" + i + "\" class=\"column\">";
-		
-		for ( z = 0; z < 10; z++) {
-
-			content +=("<div id=\"c" + i + "r" + z + "\" class=\"cell\">" + cellCounter+ "</div>");
-			
-			cellCounter++;
-
-		}
-
-		content +=("</div>");
-
-	}
-	return content;
-	
-}
-
-
-//Creates the cell array
-
-
-var createArray = function() {
-	
-	var cellArray = [];
-
-	for (i = 0; i < 10 ; i++) {
-
-		for ( z = 0; z < 10; z++) {
-
-			cellArray.push("[" + i + "][" + z + "]");
-			
-		}
-		
-	}
-	
-	return cellArray;
-}
-
-createArray();*/
-
-
