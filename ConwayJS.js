e2c4e9878fb070bec95609d5933a9425a3d1e9ba
jencoleman <jencolemanjd@gmail.com>
2015-01-08 21:39:32 -0700

    Update ConwayJS.js

--- ConwayJS.js
+++ ConwayJS.js
@@ -13,14 +13,13 @@ var createArrayGrid = function() {
 	for (i = 0; i < 10 ; i++) {
 	
 		content += "<div id=\"c" + i + "\" class=\"column\">";
-		cellArray.push([]);
 		
 		for ( z = 0; z < 10; z++) {
 	
 				content +="<div id=\"" + i + "-" + z + "\"class=\"deadCell\">" + cellCounter + "</div>";
 				cellCounter++;
-				cellArray[i].push("deadCell");
-				
+				cellArray.push("[" + i + "][" + z + "]");
+				cellArray[i][z] = "deadCell";
 			}
 			content +=("</div>");
 		}
@@ -28,18 +27,30 @@ var createArrayGrid = function() {
 	console.log(cellArray);
 	
 	}
-	
-	
-
 
 //Change cell class to live
-var itsAlive = function(){
+var itsAlive = function(a, b){
 	
-	var myCell = document.getElementById("2-2");
+	var id = a + "-" + b;
+	
+	var myCell = document.getElementById(id);
 	
 	myCell.className = "liveCell";
 	
-	cellArray[2-2] = "liveCell";
+	cellArray[a][b] = "deadCell";
+
+	}
+	
+//Start grid with acorn
+var acornAlive = function(){
+	
+	itsAlive(2,2);
+	itsAlive(2,4);
+	itsAlive(1,4);
+	itsAlive(4,3);
+	itsAlive(5,4);
+	itsAlive(6,4);
+	itsAlive(7,4);
 	
 }
 
